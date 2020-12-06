894b9fb3dc72f556e97235655fcd7c6ae38f839f
joaquintamargo <wakitamargo@gmail.com>
2020-12-06 04:49:27 +0100

    Fixed some bugs.

--- scripts/sequences-processing.sh
+++ scripts/sequences-processing.sh
@@ -82,17 +82,18 @@ mkdir Denoised
 
 ## Denoising
 
-echo "Denoising is taking place"
+echo "Denoising is taking place. This may take a while..."
+echo ""
 
 qiime dada2 denoise-single \
   --i-demultiplexed-seqs ${RESDIR}/Demux/single_end_demux.qza \
-  --p-trunc-len-f 250 \
-  --p-trunc-len-r 250 \
+  --p-trunc-len 250 \
   --o-representative-sequences ${RESDIR}/Denoised/rep-seqs-dada2.qza \
   --o-table ${RESDIR}/Denoised/table-dada2.qza \
-  --p-n-threads 8 \
+  --o-denoising-stats ${RESDIR}/Denoised/denoising-stats.qza \
   --p-max-ee 2 \
-  --p-trunc-q  2
+  --p-trunc-q  2 \
+  --verbose
  
 ## Feature table and summary 
  
@@ -156,7 +157,7 @@ qiime diversity core-metrics-phylogenetic \
   --i-phylogeny ${RESDIR}/Phylogenetic-diversity/rooted-tree.qza \
   --i-table ${RESDIR}/Denoised/table-dada2.qza \
   --p-sampling-depth 500 \
-  --m-metadata-file ${METADATADIR}/metadata.txt\
+  --m-metadata-file ${METADATADIR}/metadata.txt \
   --output-dir ${RESDIR}/Alpha-Beta-diversity/core-metrics-results
 
 ## Alpha group significance 
@@ -167,19 +168,15 @@ echo -e "\e[1mAlpha diversity is being calculated."
 echo ""
 qiime diversity alpha-group-significance \
   --i-alpha-diversity ${RESDIR}/Alpha-Beta-diversity/core-metrics-results/faith_pd_vector.qza \
-  --m-metadata-file ${METADATADIR}/meta_data_rmd.txt \
+  --m-metadata-file ${METADATADIR}/metadata.txt \
   --o-visualization ${RESDIR}/Alpha-Beta-diversity/core-metrics-results/faith-pd-group-significance.qzv
 
 qiime diversity alpha-group-significance \
   --i-alpha-diversity ${RESDIR}/Alpha-Beta-diversity/evenness_vector.qza \
-  --m-metadata-file ${METADATADIR}/meta_data_rmd.txt \
+  --m-metadata-file ${METADATADIR}/metadata.txt \
   --o-visualization ${RESDIR}/Alpha-Beta-diversity/core-metrics-results/evenness-group-significance.qzv
 
-## Beta group significance --> NECESITA REVISIÓN URGENTE PARA DETERMINAR
-## CÓMO TIENE QUE SER EL ARCHIVO DE METADATOS DE LAS MUESTRAS.
-## LOS TRES QIIME DIVERSITY DE ABAJO SON LO MISMO PERO CAMBIANDO EL
-## NOMBRE DE LOS PARAMETROS CON LOS QUE HACE LA COMPARACION
-
+## Beta group significance 
 
 echo ""
 echo -e "\e[1mBeta diversity is being calculated."
@@ -187,23 +184,9 @@ echo ""
 
 qiime diversity beta-group-significance \
   --i-distance-matrix ${RESDIR}/Alpha-Beta-diversity/core-metrics-results/unweighted_unifrac_distance_matrix.qza \
-  --m-metadata-file ${METADATADIR}/meta_data_rmd.txt \
-  --m-metadata-category Treatment \
-  --o-visualization ${RESDIR}/Alpha-Beta-diversity/core-metrics-results/unweighted-unifrac-treatment-significance.qzv \
-  --p-pairwise
-
-qiime diversity beta-group-significance \
-  --i-distance-matrix ${RESDIR}/Alpha-Beta-diversity/core-metrics-results/unweighted_unifrac_distance_matrix.qza \
-  --m-metadata-file ${METADATADIR}/meta_data_rmd.txt \
-  --m-metadata-category Time \
-  --o-visualization ${RESDIR}/Alpha-Beta-diversity/core-metrics-results/unweighted-unifrac-time-significance.qzv  \
-  --p-pairwise
-
-qiime diversity beta-group-significance \
-  --i-distance-matrix ${RESDIR}/Alpha-Beta-diversity/core-metrics-results/unweighted_unifrac_distance_matrix.qza \
-  --m-metadata-file ${METADATADIR}/meta_data_rmd.txt \
-  --m-metadata-category Section \
-  --o-visualization ${RESDIR}/Alpha-Beta-diversity/core-metrics-results/unweighted-unifrac-section-significance.qzv  \
+  --m-metadata-file ${METADATADIR}/metadata.txt \
+  --m-metadata-category Description \
+  --o-visualization ${RESDIR}/Alpha-Beta-diversity/core-metrics-results/unweighted-unifrac-description-significance.qzv \
   --p-pairwise
   
 ## Alpha rarefaction analysis 
@@ -221,12 +204,12 @@ qiime diversity alpha-rarefaction \
   --i-table ${RESDIR}/Denoised/table-dada2.qza \
   --i-phylogeny ${RESDIR}/Phylogenetic-diversity/rooted-tree.qza \
   --p-max-depth 500 \
-  --m-metadata-file ${METADATADIR}/meta_data_rmd.txt \
+  --m-metadata-file ${METADATADIR}/metadata.txt \
   --o-visualization ${RESDIR}/Alpha-rarefaction/alpha-rarefaction.qzv
 
 echo "Alpha rarefaction visualization is opening in a new window."
 
-qiime tools view  $working_dir/alpha-rarefaction.qzv
+qiime tools view  ${RESDIR}/Alpha-rarefaction/alpha-rarefaction.qzv
 
 echo ""
 echo "======================" 
@@ -235,6 +218,13 @@ echo "======================"
 echo ""
 
 echo "SILVA 16S database was chosen for the analysis, as it is an up-to-date (SILVA v.138, the newest version on 3rd or December, 2020), reliable open source database recommended by many microbiologist experts. The file that is going to be used for the alignment is stored at the Results directory that you have provided in the parameters file (${RESDIR})"
+echo ""
+
+## Creation of SILVA classifier through qiime rescript get-silva-data. 
+## HAY QUE INCLUIRLO EN LA DOCUMENTACION: RESCRIPT
+
+echo "SILVA classifier is created through a qiime script: RESCRIPt. Downloading and processing all sequences may take a while. Please, be patient."
+echo ""
 
 cd ${RESDIR}
 mkdir SILVA-classifier 
@@ -242,9 +232,14 @@ wget -O "SILVA-classifier/SILVA_138_515F_806R_seqs.qza" "https://data.qiime2.org
 mkdir Taxonomic-analysis
 
 qiime feature-classifier classify-sklearn \
-  --i-classifier SILVA-classifier/SILVA_138_515F_806R_seqs.qza \
+  --i-classifier ${RESDIR}/SILVA-classifier/SILVA_138_515F_806R_seqs.qza \
   --i-reads  ${RESDIR}/Denoised/rep-seqs-dada2.qza \
-  --o-classification ${RESDIR}/Taxonomic-analysis/taxonomy.qza
+  --o-classification ${RESDIR}/Taxonomic-analysis/taxonomy.qza \
+  --verbose
+
+echo ""
+echo "Classification of the reads by taxon using SILVA classifier has finished. Visualisation is on the way!"
+echo ""
 
 qiime metadata tabulate \
   --m-input-file ${RESDIR}/Taxonomic-analysis/taxonomy.qza \
@@ -253,7 +248,7 @@ qiime metadata tabulate \
 qiime taxa barplot \
   --i-table  ${RESDIR}/Denoised/table-dada2.qza \
   --i-taxonomy ${RESDIR}/Taxonomic-analysis/taxonomy.qza \
-  --m-metadata-file ${METADATADIR}/MappingFile_plate1_mod.txt \
+  --m-metadata-file ${METADATADIR}/metadata.txt \
   --o-visualization ${RESDIR}/Taxonomic-analysis/taxa-bar-plots.qzv
   
 echo ""
